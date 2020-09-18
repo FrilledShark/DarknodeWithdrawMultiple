5abc5da1254c65121f29a9d7c9f177736782a0b3
Alexander Lindgren <alexander@lindgren.xyz>
2020-09-18 21:52:30 +0200

    web3

--- dn.js
+++ dn.js
@@ -5,17 +5,13 @@ let app = new Vue({
         web3: "",
     },
     methods: {
-        setUpWeb3: function() {
+        setProvider: function() {
             let ethereum = window.ethereum;
-            let web3 = window.web3;
-            if (typeof ethereum !== 'undefined') {
-            await ethereum.enable();
+            ethereum.enable().then(this.setupWeb3());
+        },
+        setupWeb3: function () {
+            let ethereum = window.ethereum;
             this.web3 = new Web3(ethereum);
-            } else if (typeof web3 !== 'undefined') {
-            this.web3 = new Web3(web3.currentProvider);
-            } else {
-            this.web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER));
-            }
         }
     },
     watch: {
