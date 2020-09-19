6de37b39fd0148156607c1e8fc4f54ad31136738
Alexander Lindgren <alexander@lindgren.xyz>
2020-09-19 07:50:07 +0200

    fefs

--- dn.js
+++ dn.js
@@ -1,9 +1,11 @@
 async function sortAllDarknodes(allDarknodes, selectedAccount) {
     app.privateDarknodes = [];
+    app.fees = 0;
     for (Darknode in allDarknodes) {
         operator = await app.DarknodeRegistry.methods.getDarknodeOperator(allDarknodes[Darknode]).call();
         if (operator == selectedAccount) {
             app.privateDarknodes.push(allDarknodes[Darknode]);
+            fees += await app.DarknodePayment.methods.darknodeBalances(allDarknodes[Darknode], app.renBTC);
         }
     }
 }
@@ -13,12 +15,14 @@ let app = new Vue({
     el: '#app',
     data: {
         selectedAccount: "",
+        fees: 0,
         web3: "",
         DarknodeRegistry: "",
         DarknodePayment: "",
         allDarknodes: "",
         privateDarknodes: "",
         loadingDarknodes: "",
+        renBTC: "0xeb4c2781e4eba804ce9a9803c67d0893436bb27d",
     },
     methods: {
         setDarknodes: function (value) {
@@ -39,6 +43,8 @@ let app = new Vue({
         },
         getAllDarknodes: function() {
             this.DarknodeRegistry.methods.getDarknodes("0xe9578275A14f61f7cAF35e47ca358C7Ac89B254E", 0).call().then(function (value) {app.allDarknodes = value;});
+        },
+        getAllFees: function () {
         },
     },
     watch: {
