34177d0fef9778b9e6d292344b18716d76229eea
Alexander Lindgren <alexander@lindgren.xyz>
2020-09-19 07:56:37 +0200

    log fees

--- dn.js
+++ dn.js
@@ -5,7 +5,7 @@ async function sortAllDarknodes(allDarknodes, selectedAccount) {
         operator = await app.DarknodeRegistry.methods.getDarknodeOperator(allDarknodes[Darknode]).call();
         if (operator == selectedAccount) {
             app.privateDarknodes.push(allDarknodes[Darknode]);
-            app.fees += await app.DarknodePayment.methods.darknodeBalances(allDarknodes[Darknode], app.renBTC);
+             console.log(await app.DarknodePayment.methods.darknodeBalances(allDarknodes[Darknode], app.renBTC));
         }
     }
 }
