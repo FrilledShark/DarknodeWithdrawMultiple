68ee2b552243f01a71870e0af5ee7ca0eaa6b484
Alexander Lindgren <alexander@lindgren.xyz>
2020-09-22 08:19:17 +0200

    button

--- dn.js
+++ dn.js
@@ -85,7 +85,10 @@ let app = new Vue({
             });
         },
         easyShowEthereumAddress: function (address) {
-            return address.substring(0,7) + "..." + address.substring(address.length-5)
+            return address.substring(0,7) + "..." + address.substring(address.length-5);
+        },
+        toClipBoard: function(text) {
+            navigator.clipboard.writeText(text);
         }
     },
     watch: {
