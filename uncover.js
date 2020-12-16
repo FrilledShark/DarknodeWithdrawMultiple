19d9264e227c854b1e49a31b476113799d84d996
Lindgren <github@lindgren.xyz>
2020-12-16 13:03:13 +0100

    v2

--- uncover.js
+++ uncover.js
@@ -22,7 +22,7 @@ let app = new Vue({
             // app.web3.eth.getTransaction('tx_hash', (err,x) => tx_data = x)
             let addresses = [];
             for (let event of tx_data.logs) {
-                if (this.web3.utils.bytesToHex(event.topics[0] == "0x1619fc95050ffb8c94c9077c82b3e1ebbf8d571b6234241c55ba0aaf40da019e")) {
+                if (this.web3.utils.bytesToHex(event.topics[0]) == "0x1619fc95050ffb8c94c9077c82b3e1ebbf8d571b6234241c55ba0aaf40da019e") {
                     let addr = this.web3.utils.hexToUtf8("0x" + event.data.substring(64*3+2, 64*4+2));
                     addr = addr + this.web3.utils.hexToUtf8("0x" + event.data.substring(64*4+2, 64*5+2));
                     addresses.push(addr);
