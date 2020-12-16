72fe9e0987d672a214d35c452c1573fdd8006fbf
Lindgren <github@lindgren.xyz>
2020-12-16 12:56:22 +0100

    es

--- uncover.js
+++ uncover.js
@@ -25,7 +25,7 @@ let app = new Vue({
                 if (this.web3.utils.bytesToHex(event.topics[0] == "0x1619fc95050ffb8c94c9077c82b3e1ebbf8d571b6234241c55ba0aaf40da019e")) {
                     let addr = this.web3.utils.hexToUtf8("0x" + event.data.substring(64*3+2, 64*4+2));
                     addr = addr + this.web3.utils.hexToUtf8("0x" + event.data.substring(64*4+2, 64*5+2));
-                    address.push(addr);
+                    addresses.push(addr);
                 }
             }
             return addresses;
